aaa6b1457a38c22737bfa5d42c503862723e2348
ZapataCamilo <zapatacamilo86@gmail.com>
2023-05-13 14:17:09 -0500

    Task 1

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-apt update && upgrade -y
-apt install -y wget make dpkg curl
+sudo apt update && upgrade -y
+sudo apt install -y wget make dpkg curl
 wget -O hugo_binary.deb https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb
-dpkg -i hugo_binary.deb
+sudo dpkg -i hugo_binary.deb
 rm hugo_binary.deb
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin 
 export PATH=$PATH:$(go env GOPATH)/bin
-npm install --save-dev markdown-link-check
-npm install markdownlint --save-dev
+npm install -g markdown-link-check
+npm install -g markdownlint
 make build
