a55bc43c775e75074c6a82e9459e1ee8c4e6198b
ZapataCamilo <zapatacamilo86@gmail.com>
2023-05-19 05:45:01 -0500

    FIx: task 0

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -4,7 +4,7 @@ apt install -y wget make dpkg curl sudo
 wget -O hugo_binary.deb https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-amd64.deb
 sudo dpkg -i hugo_binary.deb
 rm hugo_binary.deb
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin 
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 sudo npm install -g markdown-link-check
